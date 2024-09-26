e9af7f6d815ecd685a8619ee080f3feb69b2f0d2
Tobias Assarsson <tobias.assarsson@gmail.com>
2024-09-26 13:38:13 +0200

    add type exports

--- src/Hooks/index.ts
+++ src/Hooks/index.ts
@@ -2,18 +2,24 @@ export * from './useMeta';
 export * from './useInstallationInfo';
 export * from './usePahoMQTTClient';
 export * from './useSimpleMQTT';
+export type {Binding, SimpleMQTT} from './useSimpleMQTT';
 
 export * from './useTokens';
 
 export * from './useNewFunction';
+export type {FunctionTemplate} from './useNewFunction';
+
 export * from './useFunctions';
 export * from './useFunction';
 
 export * from './useInstallation';
 export * from './useInstallations';
 export * from './useNewInstallation';
+export type {InstallationTemplate} from './useNewInstallation';
 
 export * from './useNewDevice';
+export type {DeviceTemplate} from './useNewDevice';
+
 export * from './useDevices';
 export * from './useDevice';
 
@@ -23,6 +29,8 @@ export * from './useEdgeApp';
 export * from './useEdgeAppVersions';
 
 export * from './useNewOAuth2Client';
+export type {OAuth2ClientTemplate} from './useNewOAuth2Client';
+
 export * from './useOAuth2Client';
 export * from './useOAuth2Clients';
 export * from './useOAuth2Consent';
@@ -31,18 +39,25 @@ export * from './useIDTokenAlgorithms';
 export * from './useOrganization';
 export * from './useOrganizations';
 export * from './useNewOrganization';
+export type {OrganizationTemplate} from './useNewOrganization';
 
 export * from './useNewUser';
+export type {UserTemplate} from './useNewUser';
+
 export * from './useUsers';
 export * from './useUser';
 
 export * from './useRoles';
 
 export * from './useNewNotificationMessage';
+export type {NotificationMessageTemplate} from './useNewNotificationMessage';
+
 export * from './useNotificationMessage';
 export * from './useNotificationMessages';
 
 export * from './useNewNotificationOutput';
+export type {NotificationOutputTemplate} from './useNewNotificationOutput';
+
 export * from './useNotificationOutput';
 export * from './useNotificationOutputs';
 
@@ -50,8 +65,11 @@ export * from './useNotificationOutputExecutor';
 export * from './useNotificationOutputExecutors';
 
 export * from './useCheckPermissions';
+export type {Permission} from './useCheckPermissions';
 
 export * from './useMQTT';
 export * from './useLiveInstallation';
+export type {LiveInstallation} from './useLiveInstallation';
 export * from './useMultiLiveInstallation';
+export type {MultiLiveInstallation} from './useMultiLiveInstallation';
 
